33c2a7c64bc8585e2d0c2e4a5e83177f4368fd0e
s0lar1s <hruskov@gmail.com>
2026-02-23 10:40:04 +0200

    fix google api

---
+++ app.config.js
@@ -0,0 +1,41 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "kome_app",
+    slug: "kome_app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.anonymous.kome-app",
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      }
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.anonymous.kome_app",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY
+        }
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    }
+  }
+};
